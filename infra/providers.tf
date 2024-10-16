31a7d74025184070f2f30628ae692bd0cb418dfb
frederic.pitteloud@epfl.ch <frederic.pitteloud@epfl.ch>
2024-10-16 09:15:45 +0200

    Create first test deployment #6 corrected providers.tf

--- infra/providers.tf
+++ infra/providers.tf
@@ -1,22 +1,27 @@
-# providers.tf
-
 terraform {
   backend "azurerm" {
     use_oidc = true
   }
 
   required_providers {
-    azurerm   = {
+    azurerm = {
       source  = "hashicorp/azurerm"
+      version = "~> 3.0"  # Specify the version if needed
     }
   }
 }
 
+provider "azurerm" {
+  features {}
+
+  # OIDC authentication (optional, see explanation below)
+  # use_oidc            = true
+  # client_id           = var.az_client_id
+  # subscription_id     = var.az_subscription_id
+  # tenant_id           = var.az_tenant_id
+}
+
 provider "azuread" {
-
-# tenant_id will be passed from main.tf or variables.tf
-
   client_id = var.az_client_id
   tenant_id = var.az_tenant_id
-
 }
